2013139ebc8ae3fd427c71c5b5287fbfa4b41a85
Saheli Mete <sahelimete97@gmail.com>
2025-04-24 13:48:21 +0530

    Added JS

--- Starter/App.js
+++ Starter/App.js
@@ -3,9 +3,45 @@ console.log("document")
 console.log(document.getElementsByTagName("span"))
 console.log(document.getElementsByTagName("h1")[0].innerHTML);
 document.getElementsByTagName("span")[0].innerHTML="I'm coming from js";
+
 document.getElementById("primary-btn-id").addEventListener('click',function(){
     alert("Hello i'm clicked");
 });
 function dontClickButtom(){
     alert("Hello i'm not clicked");
 }
+/*Bulb on to off */
+function changeImage(){
+    let bulbVal=document.getElementById("bulb");
+    if(bulbVal.src.match("ON")){
+        bulbVal.src="./assets/OFF.png";
+    }
+    else{
+        bulbVal.src="./assets/ON.png";
+    }
+}
+/*Password show to hide */
+const showPassword=()=>{
+    let inputValue=document.getElementById("inputVal");
+    let buttonValue=document.getElementById("change-id");
+    if(inputValue.type=='password'){
+        inputValue.type='text';
+        buttonValue.innerHTML='HIDE'
+    }else{
+        inputValue.type='password';
+        buttonValue.innerHTML='SHOW'
+    }
+}
+/* 
+function showPassword(){
+    let inputValue=document.getElementById("inputVal");
+    let buttonValue=document.getElementById("change-id");
+    if(inputValue.type=='password'){
+        inputValue.type='text';
+        buttonValue.innerHTML='HIDE'
+    }else{
+        inputValue.type='password';
+        buttonValue.innerHTML='SHOW'
+    }
+}
+*/
